75df622893987a96f9f364a4673e70c58ef12440
gg9192 <42902579+gg9192@users.noreply.github.com>
2022-12-27 20:14:35 -0500

    added debug mode for piece testing

--- src/ReverseZPieceTests.java
+++ src/ReverseZPieceTests.java
@@ -6,10 +6,10 @@ class ReverseZPieceTests {
 
 	/**
 	 * Test to ensure that the piece rotates correctly
-	 * from 0 to 1
+	 * from 0 to 0
 	 */
 	@Test
-	void rotate0to1() {
+	void rotate3to0() {
 		Board b = new Board(0,0);
 		Piece p = new ReverseZPiece(b);
 		Tuple[] expected = new Tuple[4];
@@ -18,7 +18,40 @@ class ReverseZPieceTests {
 		expected[2] = new Tuple(3,-2);
 		expected[3] = new Tuple(4,-2);
 		assertArrayEquals(expected, p.getPiece());
-		
 	}
 
+	/**
+	 * Test to ensure that the shadow is calculated
+	 * correctly at orientation 0
+	 */
+	@Test
+	void testShadow0() {
+		Board b = new Board(0,0);
+		b.setGrid(3, 10, 1);
+		Piece p = new ReverseZPiece(b);
+		Tuple[] expected = new Tuple[4];
+		expected[0] = new Tuple(4,8);
+		expected[1] = new Tuple(5,8);
+		expected[2] = new Tuple(3,9);
+		expected[3] = new Tuple(4,9);
+		assertArrayEquals(expected, p.getShadow());
+		b.setGrid(3, 10, 0);
+		b.setGrid(4, 10, 1);
+		p.moveLeft();
+		p.moveRight();
+		assertArrayEquals(expected, p.getShadow());
+		b.setGrid(4, 10, 0);
+		b.setGrid(5, 9, 1);
+		p.moveLeft();
+		p.moveRight();
+		assertArrayEquals(expected, p.getShadow());
+	}
+	
+	/**
+	 * Test that the shadow does not go through the floor at
+	 * orientation 1.
+	 */
+	@Test
+	void test1Floor1() {}
+	
 }
